6976eec6072da4b95339d0f99240dfec3982acd7
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-09-07 12:21:59 +0200

    deleted useless stuff

--- GameofLife/GameofLife/Board.cs
+++ GameofLife/GameofLife/Board.cs
@@ -2,18 +2,6 @@
 
 public class Board
 {
-    private bool[,] boardField;
-
-    public Board(int height, int width)
-    {
-        boardField = new bool[height, width];
-    }
-
-    public bool[,] GetBoard()
-    {
-        return boardField;
-    }
-
     public static void Print(bool[,] board)
     {
         for (var row = 0; row < board.GetLength(0); row++)
