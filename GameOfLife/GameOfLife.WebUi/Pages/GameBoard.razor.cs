f1cd1eb6cfd2b59940bbe7e274ae1d48c1b691d7
Kucher Mykyta Serhiyovich <mykyta.kucher@digitecgalaxus.ch>
2026-09-21 09:29:27 +0200

    comments resolved

---
+++ GameOfLife/GameOfLife.WebUi/Pages/GameBoard.razor.cs
@@ -0,0 +1,37 @@
+namespace GameOfLife.WebUi.Pages;
+
+using Core;
+
+public partial class GameBoard
+{
+    private int height = 20;
+    private int width = 20;
+    private int aliveCount = 100;
+
+    private bool isRunning;
+    private Game? game;
+    private bool[,]? cells;
+
+    private async Task OnConfigSubmit()
+    {
+        isRunning = false;
+        game = new Game(width, height, aliveCount);
+        await Start();
+    }
+
+    private async Task Start()
+    {
+        if (isRunning)
+            return;
+        isRunning = true;
+
+        while (isRunning)
+        {
+            cells = game!.GetNextFrame();
+            StateHasChanged();
+            await Task.Delay(200);
+        }
+    }
+
+    private void Stop() => isRunning = false;
+}
--- GameofLife/GameOfLife.Core/Board.cs
+++ GameofLife/GameOfLife.Core/Board.cs
@@ -28,11 +28,6 @@ internal class Board(int width, int height, int aliveCount)
         return board;
     }
 
-    public void Randomize()
-    {
-        board = GenerateRandomBoard(width, height, aliveCount);
-    }
-
     public void GenerateNextFrame()
     {
         var nextGen = new bool[BoardHeight, BoardWidth];
